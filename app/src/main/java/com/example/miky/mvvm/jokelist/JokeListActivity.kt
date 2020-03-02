8460393a7c98b79a7d085fe90f479576c6353fc0
Minkyu Joo <minkyu.joo@wiwa.io>
2020-03-02 13:39:10 +0900

    add cardview and drag, swipe item

--- app/src/main/java/com/example/miky/mvvm/jokelist/JokeListActivity.kt
+++ app/src/main/java/com/example/miky/mvvm/jokelist/JokeListActivity.kt
@@ -7,7 +7,9 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.miky.mvvm.R
 import com.example.miky.mvvm.databinding.ActivityJokeListBinding
 
@@ -37,6 +39,7 @@ class JokeListActivity : AppCompatActivity(), JokeListActivityInterface {
         binding.jokeList.layoutManager = layoutManager
         adapter = JokeListAdapter(this, null, onClickListItem)
         binding.jokeList.adapter = adapter
+        itemTouchHelper.attachToRecyclerView(binding.jokeList)
 
         viewModel.liveJokeList.observe(this, Observer {
             Log.i("miky", "observe~!!!! ${it.size}")
@@ -50,4 +53,25 @@ class JokeListActivity : AppCompatActivity(), JokeListActivityInterface {
     val onClickListItem = fun(view: View, index: Int) {
         viewModel.onClickItem(index)
     }
+
+    var itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            Log.i("miky", "old:${viewHolder.adapterPosition}   new: ${target.adapterPosition}")
+
+            return adapter.changeItemPosition(viewHolder.adapterPosition, target.adapterPosition)
+        }
+
+        override fun onSwiped(
+            viewHolder: RecyclerView.ViewHolder,
+            direction: Int
+        ) {
+            adapter.removeItem(viewHolder.adapterPosition)
+        }
+    })
 }
--- app/src/main/java/com/example/miky/mvvm/jokelist/JokeListAdapter.kt
+++ app/src/main/java/com/example/miky/mvvm/jokelist/JokeListAdapter.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.miky.mvvm.R
 import com.example.miky.mvvm.data.Joke
+import com.example.miky.mvvm.databinding.JokeListItemBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class JokeListAdapter(
     private var context: Context,
@@ -16,8 +20,8 @@ class JokeListAdapter(
     var itemClickListener: (View, Int) -> Unit): RecyclerView.Adapter<JokeListAdapter.JokeViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JokeViewHolder {
-        var itemView = LayoutInflater.from(context).inflate(R.layout.joke_list_item, parent, false)
-        return JokeViewHolder(itemView)
+        var binding = DataBindingUtil.inflate<JokeListItemBinding>(LayoutInflater.from(context), R.layout.joke_list_item, parent, false)
+        return JokeViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -28,20 +32,6 @@ class JokeListAdapter(
         holder.bind(position)
     }
 
-    inner class JokeViewHolder(itemView: View): ViewHolder(itemView) {
-
-        fun bind(position: Int) {
-            jokeList?.let {
-                var joke = it[position]
-                itemView.findViewById<TextView>(R.id.id_text).text = joke.id.toString()
-                itemView.findViewById<TextView>(R.id.joke_text).text = joke.joke
-                itemView.setOnClickListener {
-                    itemClickListener(it, position)
-                }
-            }
-        }
-    }
-    
     fun setList(list:ArrayList<Joke>) {
         jokeList?.let {
             it.clear()
@@ -49,8 +39,38 @@ class JokeListAdapter(
         } ?:let {
             jokeList = list
         }
-
         notifyDataSetChanged()
+    }
+
+    inner class JokeViewHolder(var binding: JokeListItemBinding): ViewHolder(binding.root) {
+
+        fun bind(position: Int) {
+            jokeList?.let {
+                var joke = it[position]
+                binding.positionText.text = position.toString()
+                binding.idText.text = joke.id.toString()
+                binding.jokeText.text = joke.joke
+                binding.root.setOnClickListener {
+                    itemClickListener(it, position)
+                }
+            }
+        }
+    }
+
+    fun changeItemPosition(oldPosition: Int, newPosition: Int): Boolean {
+        jokeList?.let {
+            Collections.swap(it, oldPosition, newPosition)
+            notifyItemMoved(oldPosition, newPosition)
+            return true
+        }
+        return false
+    }
+
+    fun removeItem(position: Int) {
+        jokeList?.let {
+            it.removeAt(position)
+            notifyItemRemoved(position)
+        }
     }
 }
 
